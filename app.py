b9a57e4dc3c23135ada1360d38b6d7ca008723ab
Xinsong Li <lixinso@gmail.com>
2021-01-01 05:34:46 +0000

    Login

--- app.py
+++ app.py
@@ -13,5 +13,22 @@ def hello_world():
     target = os.environ.get('TARGET', 'World')
     return 'Hello {}!\n'.format(target)
 
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        login_user(user)
+        flask.flash('Logged in successfully')
+
+        next = flask.request.args.get('next')
+
+        if not is_safe_url(next):
+            return flask.abort(400)
+
+        return flask.redirect(next or flask.url_for('index'))
+
+    return flask.render_template('login.html', form=form)
+
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
